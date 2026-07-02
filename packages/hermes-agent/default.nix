e239066d7ce769de9d0cf524d43e84005f5c7f4f
Sergii Maksymov <maksymov@dexor.de>
2026-07-02 03:32:47 +0200

    merge: rebase fork additions on upstream

--- packages/hermes-agent/default.nix
+++ packages/hermes-agent/default.nix
@@ -2,10 +2,22 @@
   pkgs,
   perSystem,
   flake,
+  dependencyGroups ? [
+    "gateway"
+    "misc"
+    "audio"
+  ],
+  extraDependencyGroups ? [ ],
+  extraPythonPackages ? (_: [ ]),
   ...
 }:
 pkgs.callPackage ./package.nix {
-  inherit flake;
+  inherit
+    flake
+    dependencyGroups
+    extraDependencyGroups
+    extraPythonPackages
+    ;
   inherit (pkgs) python3;
   inherit (perSystem.self) versionCheckHomeHook;
 }
