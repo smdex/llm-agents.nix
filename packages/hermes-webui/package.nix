241af1473156dbd3f3ae163fa22d8c27f4429ced
Sergii Maksymov <maksymov@dexor.de>
2026-09-03 12:30:45 +0200

    hermes-webui: init at 0.52.76

---
+++ packages/hermes-webui/package.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  flake,
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  python3,
+}:
+
+let
+  python = python3.withPackages (
+    ps: with ps; [
+      cryptography
+      pyyaml
+    ]
+  );
+in
+stdenv.mkDerivation rec {
+  pname = "hermes-webui";
+  version = "0.52.76";
+
+  src = fetchFromGitHub {
+    owner = "nesquena";
+    repo = "hermes-webui";
+    tag = "v${version}";
+    hash = "sha256-kHb+hxTpSwyxx2R+qzesDDIhHWy7oZ/r1P6d6c7Zgz8=";
+  };
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/hermes-webui $out/bin
+    cp -R . $out/share/hermes-webui
+
+    makeWrapper ${python}/bin/python $out/bin/hermes-webui \
+      --chdir $out/share/hermes-webui \
+      --set PYTHONPATH $out/share/hermes-webui \
+      --add-flags $out/share/hermes-webui/server.py
+
+    runHook postInstall
+  '';
+
+  passthru.category = "AI Assistants";
+
+  meta = with lib; {
+    description = "Browser-based control panel for Hermes Agent";
+    homepage = "https://github.com/nesquena/hermes-webui";
+    changelog = "https://github.com/nesquena/hermes-webui/releases/tag/v${version}";
+    license = licenses.mit;
+    sourceProvenance = with sourceTypes; [ fromSource ];
+    maintainers = with flake.lib.maintainers; [ smdex ];
+    platforms = platforms.unix;
+    mainProgram = "hermes-webui";
+  };
+}
